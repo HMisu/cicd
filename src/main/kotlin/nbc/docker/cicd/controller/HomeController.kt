28466395af9210e4a7f7fd980bf8a924e4d8d622
misu <misuhan33@gmail.com>
2024-11-25 15:51:51 +0900

    init commit

---
+++ src/main/kotlin/nbc/docker/cicd/controller/HomeController.kt
@@ -0,0 +1,18 @@
+package nbc.docker.cicd.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HomeController {
+
+    @GetMapping("/")
+    fun home(): String {
+        return "home"
+    }
+
+    @GetMapping("/healthz")
+    fun healthz(): String {
+        return "healthz"
+    }
+}
